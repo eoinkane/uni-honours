b8255280384bf7e42ca1494a2c8fa61f63e23797
eoinkane <eoinkane23@gmail.com>
2023-04-28 12:15:59 +0100

    Jenkins Lambda Connectivity, Reintroducing UI Cloudfront, CDK downgrade to v1 and other modifications
    cdk files in lib/ & bin/ have been updated to import for cdk v1
    there was a problem deploying v2 so I downgraded for now
    I will keep a copy of the cdk v2 files in a folder in another commit
    
    I have readded the Cloudfront UI distribution to the stack to deploy the UI
    
    package.json scripts have been updated to refer to the snake_case folder names

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import * as dotenv from 'dotenv';
 import { BackendStack } from '../lib/backend-stack';
 
--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,10 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
-import { Construct } from 'constructs';
-import { Function, Code, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { AuthorizationType, CfnMethod, LambdaRestApi, TokenAuthorizer } from 'aws-cdk-lib/aws-apigateway';
-import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Function, Code, Runtime } from '@aws-cdk/aws-lambda';
+import { AuthorizationType, CfnMethod, LambdaRestApi, TokenAuthorizer } from '@aws-cdk/aws-apigateway';
+import { BlockPublicAccess, Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import {
+  CachePolicy,
+  Distribution,
+  OriginAccessIdentity
+} from '@aws-cdk/aws-cloudfront';
+import { S3Origin, S3OriginProps } from '@aws-cdk/aws-cloudfront-origins';
+import { Vpc } from '@aws-cdk/aws-ec2';
 
 
 dotenv.config();
@@ -13,13 +19,17 @@ const cdkStack = process.env.CDK_STACK || 'BackendStack';
 const cdkId = process.env.CDK_ID || 'BackendStack';
 const authToken = process.env.AUTH_TOKEN || 'test-token';
 const jenkinsApiUrl = process.env.JENKINS_API_URL || 'url';
+const jenkinsJobName = process.env.JENKINS_JOB_NAME || 'job';
+const bitbucketApiUrl = process.env.BITBUCKET_API_URL || 'url';
+const bitbucketApiUserName = process.env.BITBUCKET_API_USER_NAME || 'url';
+const bitbucketApiAppPassword = process.env.BITBUCKET_API_APP_PASSWORD || 'url';
 const awsVpcId = process.env.AWS_VPC_ID || 'vpc-id';
 const awsSubnetName = process.env.AWS_SUBNET_NAME || 'subnet-name';
 const awsSubnetIds = (process.env.AWS_SUBNET_IDS || 'subnet-id1,subnet-id2').split(',');
 const awsAvailabilityZones = (process.env.AWS_AVAILABILITY_ZONES || 'zone-1,zone2').split(',');
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const awsVpc = Vpc.fromVpcAttributes(this, `${cdkId}AwsSubnet`, {
@@ -32,10 +42,14 @@ export class BackendStack extends cdk.Stack {
     const handlerLambda = new Function(this, `${cdkId}HandlerLambda`, {
       runtime: Runtime.PYTHON_3_9,
       handler: 'src.app.handler',
-      code: Code.fromAsset(path.join(__dirname, '../build', 'handler-lambda')),
+      code: Code.fromAsset(path.join(__dirname, '../build', 'handler_lambda')),
       functionName: `${cdkStack}-handler-lambda`,
       environment: {
-        JENKINS_API_URL: jenkinsApiUrl
+        JENKINS_API_URL: jenkinsApiUrl,
+        JENKINS_JOB_NAME: jenkinsJobName,
+        BITBUCKET_API_URL: bitbucketApiUrl,
+        BITBUCKET_API_USER_NAME: bitbucketApiUserName,
+        BITBUCKET_API_APP_PASSWORD: bitbucketApiAppPassword
       },
       vpc: awsVpc,
       vpcSubnets: { subnetGroupName: awsSubnetName },
@@ -45,7 +59,7 @@ export class BackendStack extends cdk.Stack {
     const authLambda = new Function(this, `${cdkId}AuthLambda`, {
       functionName: `${cdkStack}-auth-lambda`,
       runtime: Runtime.PYTHON_3_9,
-      code: Code.fromAsset(path.join(__dirname, '../build', 'auth-lambda')),
+      code: Code.fromAsset(path.join(__dirname, '../build', 'auth_lambda')),
       handler: 'src.app.handler',
       environment: {
         AUTH_TOKEN: authToken
@@ -79,6 +93,35 @@ export class BackendStack extends cdk.Stack {
         const methodCfn = method.node.defaultChild as CfnMethod;
         methodCfn.authorizationType = AuthorizationType.NONE;
         methodCfn.authorizerId = undefined;
+      });
+    
+      const uiBucket = new Bucket(this, `${cdkId}UiBucket`, {
+        bucketName: `${cdkStack}-ui-bucket`,
+        encryption: BucketEncryption.S3_MANAGED,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        publicReadAccess: false,
+      });
+  
+      const uiBucketOriginAccess = new OriginAccessIdentity(this, `${cdkId}UiBucketOrigin`);
+      uiBucket.grantRead(uiBucketOriginAccess);
+
+      const uiBucketOrigin = new S3Origin(
+        uiBucket,
+        {
+          originPath: "/",
+          originAccessIdentity: uiBucketOriginAccess,
+        }
+      );
+  
+      const uiCloudfront = new Distribution(this, `${cdkId}UiCloudfront`, {
+        defaultBehavior: {
+          origin: uiBucketOrigin,
+          cachePolicy: CachePolicy.CACHING_DISABLED,
+        },
+      });
+  
+      new cdk.CfnOutput(this, `${cdkId}UiCloudfrontDns`, {
+        value: uiCloudfront.distributionDomainName
       });
   }
 }
