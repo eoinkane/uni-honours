6737ad7468f3f2de178162e0c5383e8724df7095
eoinkane <eoinkane23@gmail.com>
2023-07-27 16:26:14 +0100

    spelling typo

--- backend/code/handler_lambda/src/handlers/get_mean_time_to_recovery_handler.py
+++ backend/code/handler_lambda/src/handlers/get_mean_time_to_recovery_handler.py
@@ -83,7 +83,7 @@ def get_mean_time_to_recovery_handler(event: APIGatewayProxyEvent):
         body=json.dumps(
             {
                 "meanTimeToRecoverySeconds": mean_time_to_recovery_seconds,
-                "meanTimeToRecoveryDurations": timedelta_to_string(
+                "meanTimeToRecoveryDuration": timedelta_to_string(
                     mean_time_to_recovery_timedelta
                 ),
             }
