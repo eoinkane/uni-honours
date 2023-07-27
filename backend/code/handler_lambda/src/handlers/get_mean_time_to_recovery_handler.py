da163a5374e557db1b1252794129dccc550aa676
eoinkane <eoinkane23@gmail.com>
2023-07-27 09:23:48 +0100

    Moving the get pull_requests utils to a shared file

--- backend/code/handler_lambda/src/handlers/get_mean_time_to_recovery_handler.py
+++ backend/code/handler_lambda/src/handlers/get_mean_time_to_recovery_handler.py
@@ -19,63 +19,12 @@ from ..calculators.mean_time_to_recovery import (
     filter_out_hotfix_pull_requests,
     get_timestamp_of_pr_build_of_pull_request,
 )
+from .shared import get_num_of_pull_requests, get_all_pull_requests
 
 logger = Logger(child=True)
 
 BITBUCKET_WORKSPACE = os.getenv("BITBUCKET_WORKSPACE", "workspace")
 BITBUCKET_REPO_SLUG = os.getenv("BITBUCKET_REPO_SLUG", "repo")
-
-
-def get_num_of_pull_requests():
-    num_of_pull_requests_request_url = f"/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/pullrequests?state=MERGED&fields=size"
-
-    num_of_bitbucket_pull_requests_response = make_request(
-        APIS.BITBUCKET, num_of_pull_requests_request_url
-    )
-
-    if not num_of_bitbucket_pull_requests_response["success"]:
-        logger.error(
-            "bitbucket request errored out",
-            url=num_of_pull_requests_request_url,
-            response=num_of_bitbucket_pull_requests_response,
-        )
-        raise FiveHundredError(response=num_of_bitbucket_pull_requests_response)
-
-    logger.info(
-        "successfully got the number of pull requests",
-        response=num_of_bitbucket_pull_requests_response,
-    )
-
-    try:
-        num_of_bitbucket_pull_requests = num_of_bitbucket_pull_requests_response[
-            "data"
-        ]["size"]
-    except KeyError as err:
-        raise FiveHundredError(f"Key {str(err)} cannot be found in the dict")
-
-    return num_of_bitbucket_pull_requests
-
-
-def get_all_pull_requests(number_of_pull_requests):
-    pagelen = min(50, number_of_pull_requests)
-    all_pull_requests_url = f"/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/pullrequests?state=MERGED&pagelen={pagelen}&fields=values.source.branch,values.id,values.title,values.state,values.merge_commit.hash,values.merge_commit.date,values.merge_commit.links.self.href,values.merge_commit.links.statuses.href,values.merge_commit.parents,values.merge_commit.parents.hash,values.merge_commit.parents.date,values.merge_commit.parents.links.self.href,values.merge_commit.parents.links.html.href,values.merge_commit.parents.links.statuses.href"
-
-    all_pull_request_response = make_request(APIS.BITBUCKET, all_pull_requests_url)
-
-    if not all_pull_request_response["success"]:
-        logger.error(
-            "bitbucket request errored out",
-            url=all_pull_requests_url,
-            response=all_pull_request_response,
-        )
-        raise FiveHundredError(response=all_pull_request_response)
-
-    logger.info(
-        "successfully got all of the pull requests",
-        response=all_pull_request_response,
-    )
-
-    return all_pull_request_response["data"]
 
 
 def get_mean_time_to_recovery_handler(event: APIGatewayProxyEvent):
