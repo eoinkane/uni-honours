e9b78a585c43159fb268401c6d4a6e6d6efa47fe
eoinkane <eoinkane23@gmail.com>
2023-07-28 20:12:13 +0100

    Deployment Frequency dummy data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return (<></>);
-}
+  const [deploymentFrequency, setDeploymentFrequency] = useState<any>();
+
+  useEffect(() => {
+    setDeploymentFrequency({
+        numberOfDeployments: 7,
+        latestBuildDatetime: '2023-03-10T10:10:30.000000+00:00',
+        firstBuildDatetime: '2023-03-17T10:10:30.000000+00:00',
+        timeBetweenLatestAndFirstBuild:
+          '7 days, 0 hr(s), 0 min(s), 0 sec(s)',
+    })
+  }, []);
+
+
+  return (
+    <>
+      <h2>DORA Metrics Dashboard</h2>
+      <p>Deployment Frequency</p>
+      <p>
+        Number of Deployments:{' '}
+        {deploymentFrequency?.numberOfDeployments}
+      </p>
+      <p>
+        First Deployment:{' '}
+        {new Date(
+          deploymentFrequency?.firstBuildDatetime
+        ).toUTCString()}
+      </p>
+      <p>
+        Latest Deployment:{' '}
+        {new Date(
+          deploymentFrequency?.latestBuildDatetime
+        ).toUTCString()}
+      </p>
+      <p>
+        Time Between Latest and First Deployment:
+        <br />
+        {deploymentFrequency?.timeBetweenLatestAndFirstBuild}
+      </p>
+    </>
+  );
+};
 
 export default App;
